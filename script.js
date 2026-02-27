2581ac6a6628eae390206b9d2a5d0d0799b6861a
guy-ch <guyaziada21@gmail.com>
2026-02-27 04:38:08 +0000

    mise à jour de mon portfolio et du cv

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () =>{
-    const progressBars =document.querySelectorAll('.progress');
-    progressBars.forEach(bar=>{
-        const value=bar.getAttribute('data-value');
-        bar.style.width = value + '%';
-        bar.style.width =  value + '%';
-    })
-})
